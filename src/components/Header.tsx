54b7c61ddb379c79f0f645e0967985320e387875
jayanthkavali <kavalijayanth73@gmail.com>
2025-12-19 10:15:23 +0530

    Chnaged the name of the 360° Walkthrough to 360 Viz

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,24 +10,23 @@ const Header: React.FC = () => {
     const handleScroll = () => setIsScrolled(window.scrollY > 10);
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-    
+
   }, []);
 
   return (
     <header
-      className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 ${
-        isScrolled ? 'bg-white shadow-md' : 'bg-white'
-      }`}
+      className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 ${isScrolled ? 'bg-white shadow-md' : 'bg-white'
+        }`}
     >
       <div className="container mx-auto px-6 md:px-12 py-2 flex justify-between items-center">
         {/* Logo */}
         <Link to="/" className="flex items-center space-x-3">
-        <img
-  src="/images/ipagevisionlogonew.png"
-  alt="IPage Vision Logo"
-  className="h-16 w-auto scale-150 origin-left"
+          <img
+            src="/images/ipagevisionlogonew.png"
+            alt="IPage Vision Logo"
+            className="h-16 w-auto scale-150 origin-left"
 
- />
+          />
 
         </Link>
 
@@ -52,9 +51,9 @@ const Header: React.FC = () => {
         <nav className="hidden md:flex gap-8 text-lg font-semibold text-black">
           <Link to="/#about-us" className="hover:text-orange-400 transition-colors">About us</Link>
           <Link to="/#works" className="hover:text-orange-400 transition-colors">Projects</Link>
-          <Link to="/360" className="hover:text-orange-400 transition-colors">360° Walkthrough</Link>
+          <Link to="/360" className="hover:text-orange-400 transition-colors">360°Viz</Link>
           <Link to="/#services" className="hover:text-orange-400 transition-colors">Services</Link>
-          
+
           <Link to="/#workflow" className="hover:text-orange-400 transition-colors">Workflow</Link>
           <Link to="/#clients" className="hover:text-orange-400 transition-colors">Clients</Link>
           <Link to="/#contact" className="hover:text-orange-400 transition-colors">Contact</Link>
